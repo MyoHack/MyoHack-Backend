162cfa86b1b19816633295588ef8fd5f469e5733
bmax <bmax1337@gmail.com>
2015-07-11 21:10:46 -0400

    logs

--- app/controllers/connected_controller.rb
+++ app/controllers/connected_controller.rb
@@ -7,6 +7,8 @@ class ConnectedController < WebsocketRails::BaseController
   def hit
     puts "received hit" + message
     WebsocketRails[:sound].trigger(:sound, "hit" );
+    puts "sending to sound socket"
     trigger_success "play hit"
+    puts "PLAYED HIT"
   end
 end
